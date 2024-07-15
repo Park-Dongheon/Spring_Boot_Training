7110aaea666a1365e37081c0022dc88b39850e6b
Park-Dongheon <cdsded@naver.com>
2024-07-15 23:31:45 +0900

    ?

--- Chapter03/src/main/java/com/rubypaper/controller/BoardController.java
+++ Chapter03/src/main/java/com/rubypaper/controller/BoardController.java
@@ -1,11 +1,7 @@
 package com.rubypaper.controller;
 
-import java.util.Date;
-
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.rubypaper.domain.BoardVO;
 
 @RestController
 public class BoardController {
